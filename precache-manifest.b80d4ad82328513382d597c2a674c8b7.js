254fb81b02e194883ee89af3cc4a2efb89f63296
Ihor Husar <ihor.husar@gmail.com>
2020-03-20 23:36:56 -0400

    Updates

---
+++ precache-manifest.b80d4ad82328513382d597c2a674c8b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f615807b95c4d1d8bb25b5af75fbf65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5aff33deaeeabf23b22",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3a948748669b6cc3364d",
+    "url": "/static/js/2.5496ba86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5496ba86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5aff33deaeeabf23b22",
+    "url": "/static/js/main.6b8d8316.chunk.js"
+  },
+  {
+    "revision": "4faeac592433a53e6fd6",
+    "url": "/static/js/runtime-main.7d47878e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
